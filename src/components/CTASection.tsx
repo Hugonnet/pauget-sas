96fef442ca5f4e9e5fd02462f01b99bd53b149dd
Hugonnet <e.hugonnet@free.fr>
2025-02-07 09:19:31 +0100

    Update CTASection.tsx : lien logo

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const CTASection = () => {
   return (
-    <section className="py-8">
+    <section className="py-16">
       <div className="container mx-auto px-4">
         <div className="mx-auto flex flex-col md:flex-row items-center justify-between gap-16 bg-white rounded-lg p-8 shadow-sm">
           <div className="flex flex-col items-center md:items-start">
@@ -17,10 +17,17 @@ export const CTASection = () => {
               <ArrowRight className="group-hover:translate-x-1 transition-transform duration-200" />
             </a>
           </div>
+           <a
+              href="https://www.lespinat-pauget-facades.fr"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="group inline-flex items-center space-x-2 text-lg text-primary hover:text-primary-hover transition-colors duration-200"
+            >
           <img
             src="/lovable-uploads/lespinat-pauget-logo-transparent.png"
             alt="Lespinat Pauget Facades"
             className="w-[200px] object-contain"
+            </a>
           />
         </div>
       </div>
