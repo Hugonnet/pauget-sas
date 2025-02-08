36b566cfb0be2835e2f30a91a93f2bab7d672107
Hugonnet <e.hugonnet@free.fr>
2025-02-08 15:48:33 +0100

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,6 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import Map from "@/components/Map";
+import { CTASection } from "@/components/CTASection";
 
 interface ContactFormData {
   name: string;
