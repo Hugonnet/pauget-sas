28b7210dcc317ccd4204cc18a29aed67b64d994c
Hugonnet <e.hugonnet@free.fr>
2025-02-08 12:58:58 +0100

    Update Etancheite.tsx

--- src/pages/Etancheite.tsx
+++ src/pages/Etancheite.tsx
@@ -131,7 +131,7 @@ const Etancheite = () => {
               initial={{ opacity: 0, x: 20 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.8 }}
-              className="relative h-[600px] rounded-xl overflow-hidden shadow-lg"
+              className="relative h-[400px] rounded-xl overflow-hidden shadow-lg"
             >
               <img
                 src="/lovable-uploads/Schema-etancheite-Air.jpg"
